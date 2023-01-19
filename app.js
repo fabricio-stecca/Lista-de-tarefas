8fcc979bce5ee895534bc830ac6b102becc1435b
fabricio-stecca <119526980+fabricio-stecca@users.noreply.github.com>
2023-01-19 04:45:26 -0300

    att

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+document.getElementById(`texto`).focus()
+
 
 let buttonadd = document.getElementById(`buttonadd`)
 
@@ -10,99 +12,112 @@ function adicionar() {
     var texto = document.getElementById(`texto`).value
     var resultado = document.getElementById(`resultado`)
 
-    if(texto == ``) {
+    if (texto == ``) {
 
     } else {
 
-    
-                let res = document.createElement(`div`)
-                res.setAttribute(`id`, `res${ordem}`)
-                res.setAttribute(`class`, `res`)
-                
 
-
-                let checkbox = document.createElement(`input`)
-                checkbox.setAttribute(`type`, `checkbox`)
-                checkbox.setAttribute(`id`, `checkbox${ordem}`)
-                checkbox.setAttribute(`class`, `checkbox`)
-                checkbox.setAttribute(`onclick`, `checkboxf(${ordem})`)
-                let label = document.createElement(`label`)
-                label.setAttribute(`for`, `checkbox${ordem}`)
-
-
-                let div = document.createElement(`div`)
-                div.setAttribute(`id`, `div${ordem}`)
-                div.setAttribute(`class`, `caixalista`)
+        let res = document.createElement(`div`)
+        res.setAttribute(`id`, `res${ordem}`)
+        res.setAttribute(`class`, `res`)
 
 
 
-                let p = document.createElement(`p`)
-                let txt = document.createTextNode(`${texto}`)
-                p.setAttribute(`ìd`, `p${ordem}`)
-                
+        let checkbox = document.createElement(`input`)
+        checkbox.setAttribute(`type`, `checkbox`)
+        checkbox.setAttribute(`id`, `checkbox${ordem}`)
+        checkbox.setAttribute(`class`, `checkbox`)
+        checkbox.setAttribute(`onclick`, `checkboxf(${ordem})`)
+        let label = document.createElement(`label`)
+        label.setAttribute(`for`, `checkbox${ordem}`)
 
 
-                let button = document.createElement(`input`)
-                button.setAttribute(`type`, `button`)
-                button.setAttribute(`value`, `\uD83D\uDDD1\uFE0F`)
-                button.setAttribute(`id`, `button${ordem}`)
-                button.setAttribute(`class`, `button`)
-                button.setAttribute(`onclick`, `deletar(${ordem})`)
-
-                var estilop = document.createAttribute(`style`)
-                estilop.value = ``
+        let div = document.createElement(`div`)
+        div.setAttribute(`id`, `div${ordem}`)
+        div.setAttribute(`class`, `caixalista`)
 
 
-                resultado.appendChild(res)
-                p.appendChild(txt)
-                label.appendChild(p)
-                div.appendChild(label)
-                res.appendChild(checkbox)
-                res.appendChild(div)
-                res.appendChild(button)
 
-                ordem = ordem + 1
-                document.getElementById(`texto`).value = ``
-                document.getElementById(`texto`).focus()
-
-    }
-
-                    
-
-    }
-
-    function deletar(cont) {
-
-        var resex = window.document.getElementById(`res${cont}`)
-        resex.remove()
-        
-
-    }
-
-    function checkboxf(cont2) {
-
-        var check = window.document.getElementById(`checkbox${cont2}`)
-        var resex = window.document.getElementById(`res${cont2}`)
-        var divex = window.document.getElementById(`div${cont2}`)
+        let p = document.createElement(`p`)
+        let txt = document.createTextNode(`${texto}`)
+        p.setAttribute(`ìd`, `p${ordem}`)
 
 
-        if(check.checked) {
-            divex.style.textDecoration = "line-through";
-            divex.style.backgroundColor= "rgb(175, 175, 255)";
-        } else {
-            divex.style.textDecoration = "none";
-            divex.style.backgroundColor= "rgb(97, 97, 255)"
-        }
+
+        let button = document.createElement(`input`)
+        button.setAttribute(`type`, `button`)
+        button.setAttribute(`value`, `\uD83D\uDDD1\uFE0F`)
+        button.setAttribute(`id`, `button${ordem}`)
+        button.setAttribute(`class`, `button`)
+        button.setAttribute(`onclick`, `deletar(${ordem})`)
+
+        var estilop = document.createAttribute(`style`)
+        estilop.value = ``
 
 
- 
-  
+        resultado.appendChild(res)
+        p.appendChild(txt)
+        label.appendChild(p)
+        div.appendChild(label)
+        res.appendChild(checkbox)
+        res.appendChild(div)
+        res.appendChild(button)
 
+        ordem = ordem + 1
+        document.getElementById(`texto`).value = ``
+        document.getElementById(`texto`).focus()
 
     }
 
 
-   
+
+}
+
+function deletar(cont) {
+
+    var resex = window.document.getElementById(`res${cont}`)
+    resex.remove()
+    document.getElementById(`texto`).focus()
+
+
+}
+
+function checkboxf(cont2) {
+
+    var check = window.document.getElementById(`checkbox${cont2}`)
+    var resex = window.document.getElementById(`res${cont2}`)
+    var divex = window.document.getElementById(`div${cont2}`)
+
+
+    if (check.checked) {
+        divex.style.textDecoration = "line-through";
+        divex.style.backgroundColor = "rgb(175, 175, 255)";
+    } else {
+        divex.style.textDecoration = "none";
+        divex.style.backgroundColor = "rgb(97, 97, 255)"
+    }
+
+    document.getElementById(`texto`).focus()
+
+
+}
+
+function pressenter() {
+
+    let texto = document.getElementById(`texto`)
+
+    texto.addEventListener(`keypress`, (e) => {
+
+        e.keyCode == 13 ? adicionar() : false
+
+
+    })
+}
+
+pressenter()
+
+
+
 
 
 
